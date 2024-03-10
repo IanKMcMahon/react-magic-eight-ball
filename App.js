681b44c123a72918ce82318e5a7e035ead43641c
Ian McMahon <iankmcmahon@gmail.com>
2024-03-09 16:10:04 -0800

    Full Exercise

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import EightBall from "./EightBall";
+
+const App = () => {
+  return <EightBall />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
